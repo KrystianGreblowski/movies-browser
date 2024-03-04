831fafd1ed4c7bde26f821d592035093be75864c
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-04 22:46:29 +0100

    GlobalStyle added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import App from "./App";
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   </ThemeProvider>
