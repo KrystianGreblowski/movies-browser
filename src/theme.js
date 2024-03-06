4969f2fa492f3e4e8d7081b917f07cb9db70d64d
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-06 18:13:04 +0100

    added colors and breakpoints  to theme

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,15 @@
 export const theme = {
-  color: {},
-  breakpoint: {},
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    waterloo: "#7E839A",
+    mystic: "#E4E6F0",
+    woodsmoke: "#18181B",
+    silver: "#ffffff6b",
+  },
+  breakpoint: {
+    mobile: "767px",
+    tablet: "1140px",
+    narrow: "1520px",
+  },
 };
