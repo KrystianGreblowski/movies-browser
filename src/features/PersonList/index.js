9e16f000165849257cc78b70797b67349512f9de
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-06 23:43:50 +0100

    PersonList added with one tile

---
+++ src/features/PersonList/index.js
@@ -0,0 +1,8 @@
+import PersonTile from "./PersonTile";
+import personImage from "./images/person-image.png";
+
+const PersonList = () => {
+  return <PersonTile image={personImage} name={"Jason Scott Lee Long name"} />;
+};
+
+export default PersonList;
