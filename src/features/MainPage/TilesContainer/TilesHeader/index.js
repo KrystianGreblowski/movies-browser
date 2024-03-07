ca95596e22b44de3656f92e71331644d8ebe12c0
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-07 14:25:03 +0100

    Added media query to TilesHeader

--- src/features/MainPage/TilesContainer/TilesHeader/index.js
+++ src/features/MainPage/TilesContainer/TilesHeader/index.js
@@ -2,4 +2,9 @@ import styled from "styled-components";
 
 export const TilesHeader = styled.h1`
     font-size: 36px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        font-size: 18px;
+        margin-left: 18px;
+    }
 `;
