bc36b83648a55b1d9626e2ab44df025d33bae131
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-08 18:12:27 +0100

    router and navigation had been added to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import {
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
 import NavigationBar from "./common/NavigationBar/index";
 import MainPage from "./features/MainPage";
+import PersonList from "./features/PersonList";
+import { toMainPage, toPersonList } from "./routes";
 
 function App() {
   return (
-    <>
-      <MainPage />
+    <HashRouter>
       <NavigationBar />
-    </>
-  )
+      <Switch>
+        <Route path={toMainPage()}>
+          <MainPage />
+        </Route>
+        <Route path={toPersonList()}>
+          <PersonList />
+        </Route>
+        <Route path="/">
+          <Redirect to="/movies" />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default App;
