63818a531fea13dcebd51c1be3a50bd2501fc85a
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-09 15:07:31 +0100

    MoviePage created

---
+++ src/features/MoviePage/index.js
@@ -0,0 +1,14 @@
+import Banner from "./Banner";
+import Content from "./Content";
+import { Container } from "./Container";
+
+function MoviePage() {
+  return (
+    <Container>
+      <Banner />
+      <Content />
+    </Container>
+  );
+}
+
+export default MoviePage;
