51400dcdbbb98e67ed4f26f77f4f643a604ea57c
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-09 10:40:04 +0100

    theme updated

--- src/theme.js
+++ src/theme.js
@@ -2,13 +2,16 @@ export const theme = {
   color: {
     white: "#FFFFFF",
     whisper: "#F5F5FA",
-    white: "#FFFFFF",
-    waterloo: "#7B7C94",
+    waterloo: "#7E839A",
+    mystic: "#E4E6F0",
     woodsmoke: "#18181B",
+    silver: "#ffffff6b",
+    black: "#000000",
   },
   breakpoint: {
-    mobile: "320px",
-    tablet: "480px",
-    narrow: "768px",
+    smallMobile: "320px",
+    mobile: "767px",
+    tablet: "1024px",
+    narrow: "1536px",
   },
 };
