ff87b76ee02c64d78e48bd45388edc04260f773a
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-09 14:01:42 +0100

    NavigationBar hidden

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import NavigationBar from "./common/NavigationBar/index";
+// import NavigationBar from "./common/NavigationBar/index";
 // import MainPage from "./features/MainPage";
 import PersonList from "./features/PersonList";
 
 function App() {
   return (
     <>
-      <NavigationBar />
+      {/* <NavigationBar /> */}
       {/* <MainPage />; */}
       <PersonList />
     </>
