d34422166eaa89d186865a3e36f6c348d2a2d0ba
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-10 17:23:35 +0100

    popular movies api added

--- src/features/MainPage/index.js
+++ src/features/MainPage/index.js
@@ -1,15 +1,28 @@
 import { Container } from "./Container";
 import { TilesContainer } from "./TilesContainer";
-import { ExampleTiles } from "./TilesContainer/Tiles";
-import { TilesHeader } from "./TilesContainer/TilesHeader";
+import { MovieTile } from "./TilesContainer/MovieTile";
+import { TilesHeader } from "./TilesHeader";
+import { usePopularMovies } from "./usePopularMovies";
 
 function MainPage() {
+  const popularMoviesData = usePopularMovies(1);
+  const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
+
   return (
     <Container>
+      <TilesHeader>Popular movies</TilesHeader>
       <TilesContainer>
-        <TilesHeader>Popular movies</TilesHeader>
-        <ExampleTiles />
-        <ExampleTiles />
+        {popularMoviesData.map((popularMovies) => (
+          <MovieTile
+            key={popularMovies.id}
+            image={imageBaseUrl + popularMovies.poster_path}
+            title={popularMovies.title}
+            year={popularMovies.release_date.slice(0, 4)}
+            type={"drama"}
+            rate={popularMovies.vote_average.toFixed(1)}
+            votes={popularMovies.vote_count}
+          />
+        ))}
       </TilesContainer>
     </Container>
   );
