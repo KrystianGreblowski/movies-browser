f7f246540ef31ca72c9c15bd6de9f55e4dad595a
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-10 17:21:17 +0100

    file moved to other folder and props added

---
+++ src/features/MainPage/TilesContainer/MovieTile/index.js
@@ -0,0 +1,37 @@
+import {
+  Tiles,
+  Tile,
+  Image,
+  Description,
+  MovieTitle,
+  MovieYear,
+  MovieTypes,
+  MovieType,
+  Rating,
+  StarImage,
+  Rate,
+  Votes,
+} from "./styled";
+import star from "../.././Images/Vector.svg";
+
+export const MovieTile = ({ image, title, year, type, rate, votes }) => {
+  return (
+    <Tiles>
+      <Tile>
+        <Image src={image} />
+        <Description>
+          <MovieTitle>{title}</MovieTitle>
+          <MovieYear>{year}</MovieYear>
+          <MovieTypes>
+            <MovieType> {type} </MovieType>
+          </MovieTypes>
+          <Rating>
+            <StarImage src={star} />
+            <Rate>{rate}</Rate>
+            <Votes>{votes}</Votes>
+          </Rating>
+        </Description>
+      </Tile>
+    </Tiles>
+  );
+};
