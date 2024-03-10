113929a2dbbea102be9780f1275f68f3944904b7
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-10 15:58:55 +0100

    Header example deleted

--- src/features/MainPage/index.js
+++ src/features/MainPage/index.js
@@ -1,21 +1,18 @@
 import { Container } from "./Container";
-import { Header } from "./Header";
 import { TilesContainer } from "./TilesContainer";
 import { ExampleTiles } from "./TilesContainer/Tiles";
 import { TilesHeader } from "./TilesContainer/TilesHeader";
 
-
 function MainPage() {
-    return (
-        <Container>
-            <Header />
-            <TilesContainer>
-                <TilesHeader>Popular movies</TilesHeader>
-                <ExampleTiles />
-                <ExampleTiles />
-            </TilesContainer>
-        </Container>
-    )
+  return (
+    <Container>
+      <TilesContainer>
+        <TilesHeader>Popular movies</TilesHeader>
+        <ExampleTiles />
+        <ExampleTiles />
+      </TilesContainer>
+    </Container>
+  );
 }
 
 export default MainPage;
