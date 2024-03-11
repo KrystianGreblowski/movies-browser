b9327254fb3c965ec671614be7a52f170ef3d0ac
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-11 18:15:55 +0100

    Component Cast created with stilization

---
+++ src/features/MoviePage/Content/Crew/index.js
@@ -0,0 +1,22 @@
+import PersonTile from "../../../../common/Tiles/PersonTile/TilesContainer/PersonsTile";
+import PersonsTitle from "../../../../common/Tiles/PersonTile/TilesContainer/PersonsTitle";
+import { Tiles } from "../../../../common/Tiles/PersonTile/TilesContainer";
+import personImage from "../../../../images/person-crew.png";
+
+const Crew = () => {
+  return (
+    <>
+      <PersonsTitle title={"Crew"} />
+      <Tiles>
+        <PersonTile image={personImage} name={"Liu Yifei"} />
+        <PersonTile image={personImage} name={"Jason Scott Lee Long name"} />
+        <PersonTile image={personImage} name={"Jason Scott Lee Long name"} />
+        <PersonTile image={personImage} name={"Jason Scott Lee Long name"} />
+        <PersonTile image={personImage} name={"Jason Scott Lee Long name"} />
+        <PersonTile image={personImage} name={"Liu Yifei"} />
+      </Tiles>
+    </>
+  );
+};
+
+export default Crew;
