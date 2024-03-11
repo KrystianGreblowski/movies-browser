b4752e7be7ef23b75db3ff7150e888e8b81164f8
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-03-11 21:11:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
 import NavigationBar from "./common/NavigationBar/index";
 import MainPage from "./features/MainPage";
 import PersonList from "./features/PersonList";
+import { toMainPage, toPersonList } from "./routes";
 
 function App() {
   return (
