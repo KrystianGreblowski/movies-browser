b62d101d214574903b0d2b5bb62c038a465f68ba
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-13 19:05:00 +0100

    Container added, components Votes & StarImage moved to RateContainer

--- src/features/MoviePage/Banner/index.js
+++ src/features/MoviePage/Banner/index.js
@@ -1,7 +1,10 @@
 import {
+  ImagesWrapper,
+  OneMoreWrapper,
   Poster,
   PosterContainer,
   Rate,
+  RateContainer,
   RateMax,
   Rating,
   Shadow,
@@ -17,18 +20,21 @@ import shadow from "../../../images/Shadow.png";
 const Banner = () => {
   return (
     <PosterContainer>
-      <Poster src={bigPoster} alt="Poster" />
-      <Shadow src={shadow} alt="Shadow Frame" />
-      <TitleWrapper>
-        <Title>Mulan long title</Title>
-        <Rating>
-          <StarImage src={star} />
-          <Rate>7,9</Rate>
-          <RateMax>/ 10</RateMax>
-          
-        </Rating>
-        <Votes>335 votes</Votes>
-      </TitleWrapper>
+      <ImagesWrapper>
+        <Poster src={bigPoster} alt="Poster" />
+        <Shadow src={shadow} alt="Shadow Frame" />
+        <TitleWrapper>
+          <Title>Mulan long title</Title>
+          <Rating>
+            <StarImage src={star} />
+            <RateContainer>
+              <Rate>7,9</Rate>
+              <RateMax>/ 10</RateMax>
+              <Votes>335 votes</Votes>
+            </RateContainer>
+          </Rating>
+        </TitleWrapper>
+      </ImagesWrapper>
     </PosterContainer>
   );
 };
