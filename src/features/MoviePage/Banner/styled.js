4f47f3b0aab685db69b4fa53804589adb0e25658
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-13 19:05:20 +0100

    Media queries created, stilization for new container added

--- src/features/MoviePage/Banner/styled.js
+++ src/features/MoviePage/Banner/styled.js
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 export const PosterContainer = styled.div`
+  margin: 0 auto;
+  background-color: ${({ theme }) => theme.color.black};
+`;
+export const ImagesWrapper = styled.div`
+  max-width: 1356px;
   position: relative;
-  max-width: 1920px;
   max-height: 770px;
-  margin: auto;
+  margin: 0 auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.narrow}) {
+    width: 90%;
+  }
 `;
 
 export const Shadow = styled.img`
-  width: 100%;
+  max-width: 1356px;
   height: 100%;
   z-index: 1;
   position: absolute;
@@ -17,55 +25,135 @@ export const Shadow = styled.img`
 `;
 
 export const Poster = styled.img`
-  max-width: 1368px;
-  max-height: 770px;
-  margin: 0 auto;
-  display: flex;
+  max-width: 1356px;
   z-index: 2;
-  
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    width: 100%;
+  }
 `;
 
 export const TitleWrapper = styled.div`
   color: ${({ theme }) => theme.color.white};
-  bottom: 56px;
-  left: 276px;
+  bottom: 0px;
+  left: 0px;
   z-index: 3;
   position: absolute;
   text-align: left;
-  display: flex;
-  flex-direction: column;
+  padding-bottom: 56px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    padding-bottom: 0px;
+  }
 `;
 
 export const Title = styled.div`
   font-size: 64px;
   font-weight: 600;
-  line-height: 77px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    font-size: 58px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 48px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 34px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
+    font-size: 30px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.smallMobile}) {
+    font-size: 24px;
+  }
 `;
 
 export const Rating = styled.div`
+  max-width: 130px;
   display: flex;
   align-items: center;
-  gap: 8px;
-  margin-top: -20px;
+  flex-wrap: wrap;
+  margin: 20px 0;
+  gap: 6px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    margin: 5px 0;
+    max-width: 100%;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    
+  }
 `;
+
+
 
 export const StarImage = styled.img`
   width: 40px;
   height: 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    width: 32px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 26px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    width: 22px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.smallMobile}) {
+    width: 16px;
+  }
 `;
 
 export const Rate = styled.p`
   font-size: 30px;
   font-weight: 500;
-  line-height: 39px;
+
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    font-size: 25px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 20px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 17px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.smallMobile}) {
+    font-size: 14px;
+  }
 `;
 
 export const RateMax = styled.p`
   font-size: 16px;
-  line-height: 19px;
-  padding-top: 10.7px;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    font-size: 13px;
+  }
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.smallMobile}) {
+    font-size: 10px;
+  }
 `;
 
 export const Votes = styled.p`
-  margin: 0;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}) {
+    font-size: 14px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 12px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.smallMobile}) {
+    font-size: 10px;
+  }
 `;
