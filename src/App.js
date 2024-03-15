72228be3ab3441afa2014d3abbd355d66dea84e0
Oleksandr Riaboshapka <hadion@mail.ru>
2024-03-15 16:23:48 +0100

    App.js for checking how looks like MoviePage added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import {
-  HashRouter,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom";
-import NavigationBar from "./common/NavigationBar/index";
-import MainPage from "./features/MainPage";
-import PersonList from "./features/PersonList";
-import { toMainPage, toPersonList } from "./routes";
+import MoviePage from "./features/MoviePage";
 
 function App() {
   return (
-    <HashRouter>
-      <NavigationBar />
-      <Switch>
-        <Route path={toMainPage()}>
-          <MainPage />
-        </Route>
-        <Route path={toPersonList()}>
-          <PersonList />
-        </Route>
-        <Route path="/">
-          <Redirect to="/movies" />
-        </Route>
-      </Switch>
-    </HashRouter>
+    <>
+      <MoviePage />
+    </>
   );
 }
 
