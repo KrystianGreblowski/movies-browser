15058417a4128de59c9b446d64ac7e1267c91bb5
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 13:48:52 +0100

    padding and margin-bottom added

--- src/features/MainPage/Container/index.js
+++ src/features/MainPage/Container/index.js
@@ -3,4 +3,10 @@ import styled from "styled-components";
 export const Container = styled.main`
   max-width: 1368px;
   margin: 0 auto;
+  padding: 0 16px;
+  margin-bottom: 123px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+    margin-bottom: 41px;
+  }
 `;
