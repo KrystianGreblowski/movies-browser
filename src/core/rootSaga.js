94301f01aea4221981b3f329f5a194158fe39700
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 19:34:04 +0100

    rootSaga added

---
+++ src/core/rootSaga.js
@@ -0,0 +1,6 @@
+import { all } from "redux-saga/effects";
+import { watchFetchPopularMovies } from "../features/MainPage/popularMoviesSaga";
+
+export default function* rootSaga() {
+  yield all([watchFetchPopularMovies()]);
+}
