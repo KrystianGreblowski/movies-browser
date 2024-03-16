74e5a377d0dec4f71ecf13dfa30f86da9aeb2bac
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 15:40:17 +0100

    store updated

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import popularMoviesReducer from "./features/MainPage/popularMoviesSlice";
+import paginationReducer from "./common/Pagination/paginationSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: { popularMovies: popularMoviesReducer },
+  reducer: {
+    popularMovies: popularMoviesReducer,
+    pagination: paginationReducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
 });
