1b62222898fdb17f32625401bb8e3ce4e76dd3c7
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 13:48:31 +0100

    height deleted and margin-bottom added

--- src/features/MainPage/TilesContainer/index.js
+++ src/features/MainPage/TilesContainer/index.js
@@ -2,9 +2,13 @@ import styled from "styled-components";
 
 export const TilesContainer = styled.article`
   max-width: 1368px;
-  height: 1391px;
-  margin: auto;
+  margin: 0 auto;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 24px;
+  margin-bottom: 56px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+    margin-bottom: 50px;
+  }
 `;
