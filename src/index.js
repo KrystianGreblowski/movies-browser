7f004b0b21fd458499afef7735821c8ce798ce87
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 19:42:56 +0100

    files moved to core folder

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import App from "./App";
-import { GlobalStyle } from "./GlobalStyle";
-import store from "./store";
+import { theme } from "./core/theme";
+import App from "./core/App";
+import { GlobalStyle } from "./core/GlobalStyle";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchPopularMovies } from "./features/MainPage/popularMoviesSaga";
-
-export default function* rootSaga() {
-  yield all([watchFetchPopularMovies()]);
-}
