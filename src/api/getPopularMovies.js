44cba0433ffd9567c6e8c420960cdc3f795d2f87
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-16 12:50:03 +0100

    try and catch deleted from getPopularMovie

--- src/api/getPopularMovies.js
+++ src/api/getPopularMovies.js
@@ -1,19 +1,10 @@
 import axios from "axios";
 
 export const getPopularMovies = async (pageNumber) => {
-  try {
-    const apiKey = "41ffdcbc63d2d58e19c2ae7bdcfaef4d";
-    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${pageNumber}`;
+  const apiKey = "41ffdcbc63d2d58e19c2ae7bdcfaef4d";
+  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${pageNumber}`;
 
-    const response = await axios.get(apiUrl);
+  const response = await axios.get(apiUrl);
 
-    if (response.status !== 200) {
-      throw new Error(response.statusText);
-    }
-
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(error.message);
-  }
+  return response.data;
 };
