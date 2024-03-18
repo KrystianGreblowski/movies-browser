9c0fb88bd8d0fb8c4677114fdf8038546d1b4b60
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-03-18 19:32:21 +0100

    TilesContainer created in common/PersonTile

---
+++ src/common/Tiles/PersonTile/TilesContainer/index.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  margin-bottom: 56px;
+  padding: 0 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+    margin-bottom: 50px;
+  }
+`;
+
+export const Tiles = styled.div`
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  justify-items: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    grid-gap: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-gap: 16px;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
+    grid-gap: 16px;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+    grid-gap: 16px;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
