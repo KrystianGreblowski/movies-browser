6aac5e577dfc177040bb5f080b30c1be15039f37
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-03-18 19:02:17 +0100

    App.js returned as was

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import MoviePage from "./features/MoviePage";
+import {
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
+import NavigationBar from "./common/NavigationBar/index";
+import MainPage from "./features/MainPage";
+import PersonList from "./features/PersonList";
+import { toMainPage, toPersonList } from "./routes";
 
 function App() {
   return (
-    <>
-      <MoviePage />
-    </>
+    <HashRouter>
+      <NavigationBar />
+      <Switch>
+        <Route path={toMainPage()}>
+          <MainPage />
+        </Route>
+        <Route path={toPersonList()}>
+          <PersonList />
+        </Route>
+        <Route path="/">
+          <Redirect to="/movies" />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
