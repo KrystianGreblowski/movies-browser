e3098219c33124e982cf328a57ee471812bf6529
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-03-23 18:49:06 +0100

    query, useLocation, onInputChange added

--- src/common/NavigationBar/SearchBar/index.js
+++ src/common/NavigationBar/SearchBar/index.js
@@ -1,10 +1,30 @@
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { Icon, Input, StyledSearchBar } from "./styled";
 
 const SearchBar = () => {
+  const query = useQueryParameter("search");
+  const replaseQueryParameter = useReplaceQueryParameter();
+  const location = useLocation();
+  const isMoviePage = location.pathname.startsWith("/movies");
+
+  const onInputChange = ({ target }) => {
+    replaseQueryParameter({
+      key: "search",
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
+  };
+
   return (
     <StyledSearchBar>
       <Icon />
-      <Input placeholder={"Search for movies..."}/>
+      <Input
+        placeholder={
+          isMoviePage ? "Search for movies..." : "Search for people..."
+        }
+        value={query || ""}
+        onChange={onInputChange}
+      />
     </StyledSearchBar>
   );
 };
