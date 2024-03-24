5220b6ae711b64b0da9b7aacdb03e98cac64f276
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 11:18:57 +0100

    max-width changed to 1400px and padding: 0 16px added

--- src/features/MoviePage/Content/styled.js
+++ src/features/MoviePage/Content/styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const MoviesContainer = styled.div`
-  max-width: 1368px;
+  max-width: 1400px;
+  padding: 0 16px;
   margin: 56px auto;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.narrow}) {
