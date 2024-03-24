c52cedb8dd2a42ff53c0966222df029bba2c6667
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 16:56:31 +0100

    extraInfo added

--- src/common/Tiles/PersonTilesContainer/PersonTile/index.js
+++ src/common/Tiles/PersonTilesContainer/PersonTile/index.js
@@ -1,11 +1,12 @@
-import { TileButton, Tile, Image, Name } from "./styled";
+import { TileButton, Tile, Image, Name, ExtraInfo } from "./styled";
 
-const PersonTile = ({ image, name }) => {
+const PersonTile = ({ image, name, extraInfo }) => {
   return (
     <TileButton>
       <Tile>
         <Image src={image} alt={name}></Image>
         <Name>{name}</Name>
+        {extraInfo && <ExtraInfo>{extraInfo}</ExtraInfo>}
       </Tile>
     </TileButton>
   );
