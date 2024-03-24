690ac8a8d24113bafc7139d3e42b413ce577796a
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 13:54:21 +0100

    getMovieDetails added

---
+++ src/api/getMovieDetails.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getMovieDetails = async () => {
+  const apiKey = "41ffdcbc63d2d58e19c2ae7bdcfaef4d";
+  const language = "en-US";
+  const movieId = 1011985;
+
+  const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=${language}`;
+
+  const response = await axios.get(apiUrl);
+
+  return response.data;
+};
