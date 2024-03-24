6790e8ccf7f10e2671c37a98953c50fdf647a370
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 12:57:33 +0100

    query language added

--- src/api/getMovieTypes.js
+++ src/api/getMovieTypes.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 export const getMovieTypes = async () => {
   const apiKey = "41ffdcbc63d2d58e19c2ae7bdcfaef4d";
+  const language = "en";
+
   const apiUrl = `
-  https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
+  https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=${language}`;
 
   const response = await axios.get(apiUrl);
 
