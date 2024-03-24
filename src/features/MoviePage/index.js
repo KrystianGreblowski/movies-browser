f05fffa73d0b9028734e5958029cce5af547d987
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 14:16:09 +0100

    movie details init added (for example movie)

--- src/features/MoviePage/index.js
+++ src/features/MoviePage/index.js
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import Banner from "./Banner";
 import Content from "./Content";
 import { Container } from "./styled";
+import { fetchMovieDetailsInit } from "./movieDetailsSlice";
+import { useDispatch } from "react-redux";
 
 function MoviePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMovieDetailsInit());
+  }, [dispatch]);
+
   return (
     <Container>
       <Banner />
