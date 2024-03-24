28f9c848608e423d6bd6e6bfa96a6f33589b6c8a
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 14:21:18 +0100

    data from Api for Banner added

--- src/features/MoviePage/Banner/index.js
+++ src/features/MoviePage/Banner/index.js
@@ -12,22 +12,34 @@ import {
   Votes,
 } from "./styled";
 import star from "../../../images/Vector.svg";
-import bigPoster from "../../../images/Poster-big.png";
 import shadow from "../../../images/Shadow.png";
+import { useSelector } from "react-redux";
+import { selectMovieDetailsData } from "../movieDetailsSlice";
 
 const Banner = () => {
+  const movieDetailsData = useSelector(selectMovieDetailsData);
+  const posterBaseUrl = "http://image.tmdb.org/t/p/original";
+
   return (
     <PosterContainer>
       <ImagesWrapper>
-        <Poster src={bigPoster} alt="Poster" />
-        <Shadow src={shadow} alt="Shadow Frame" />
+        <Poster
+          src={posterBaseUrl + movieDetailsData.backdrop_path}
+          alt={movieDetailsData.original_title}
+        />
+        <Shadow src={shadow} alt={"Shadow Frame"} />
         <TitleWrapper>
-          <Title>Mulan long title</Title>
+          <Title>{movieDetailsData.original_title}</Title>
           <Rating>
             <StarImage src={star} />
-            <Rate>7,9</Rate>
+            <Rate>
+              {movieDetailsData.vote_average
+                .toFixed(1)
+                .toString()
+                .replace(".", ",")}
+            </Rate>
             <RateMax>/ 10</RateMax>
-            <Votes>335 votes</Votes>
+            <Votes>{movieDetailsData.vote_count} votes</Votes>
           </Rating>
         </TitleWrapper>
       </ImagesWrapper>
