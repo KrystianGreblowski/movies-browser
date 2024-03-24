47123e6c33521bfe50b59e803e092d452e636e3c
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-24 11:23:22 +0100

    breakpoint deleted

--- src/features/MoviePage/Content/styled.js
+++ src/features/MoviePage/Content/styled.js
@@ -4,8 +4,4 @@ export const MoviesContainer = styled.div`
   max-width: 1400px;
   padding: 0 16px;
   margin: 56px auto;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.narrow}) {
-    width: 95%;
-  }
 `;
