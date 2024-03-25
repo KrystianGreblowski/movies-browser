f33b686b6c40363ffe39c7530ae31266a659ee79
Damian Niklasinski <damianiklasinski@wp.pl>
2024-03-25 21:36:52 +0100

    App restored to previous version

--- src/core/App.js
+++ src/core/App.js
@@ -1,21 +1,21 @@
 import {
   HashRouter,
-  //Redirect,
-  //Route,
-  //Switch,
+  Redirect,
+  Route,
+  Switch,
 } from "react-router-dom/cjs/react-router-dom";
 import NavigationBar from "../common/NavigationBar";
 import NoResults from "../features/NoResults";
-// import MainPage from "../features/MainPage";
-// import PersonList from "../features/PersonList";
-// import { toMainPage, toPersonList } from "./routes";
-// import MoviesDetails from "../features/MoviesDetails"
+import MainPage from "../features/MainPage";
+import PersonList from "../features/PersonList";
+import { toMainPage, toPersonList } from "./routes";
+import MoviesDetails from "../features/MoviesDetails";
 
 function App() {
   return (
     <HashRouter>
       <NavigationBar />
-      {/*<Switch>
+      <Switch>
         <Route path="/movies/:title">
           <MoviesDetails />
         </Route>
@@ -28,8 +28,7 @@ function App() {
         <Route path="/">
           <Redirect to="/movies" />
         </Route>
-  </Switch>*/}
-    <NoResults />
+      </Switch>
     </HashRouter>
   );
 }
