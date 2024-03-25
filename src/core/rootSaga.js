3fa84dfa58a43877177922f2dcbeb349d4fa78b7
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-03-25 20:45:31 +0100

    watchFetchMovieTypes renamed to watchFetchMovieTypesInit

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,14 +1,14 @@
 import { all } from "redux-saga/effects";
 import { watchFetchCurrentMoviesPage } from "../features/MainPage/popularMoviesSaga";
 import { watchFetchCurrentPeoplePage } from "../features/PersonList/popularPeopleSaga";
-import { watchFetchMovieTypes } from "../common/movieTypes/movieTypesSaga";
+import { watchFetchMovieTypesInit } from "../common/movieTypes/movieTypesSaga";
 import { watchFetchMovieDetails } from "../features/MoviePage/movieDetailsSaga";
 
 export default function* rootSaga() {
   yield all([
     watchFetchCurrentMoviesPage(),
     watchFetchCurrentPeoplePage(),
-    watchFetchMovieTypes(),
+    watchFetchMovieTypesInit(),
     watchFetchMovieDetails(),
   ]);
 }
