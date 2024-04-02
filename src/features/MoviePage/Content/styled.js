7f4216466062c7bdb291ac4df313157bae3b8ce8
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-02 22:16:31 +0200

    margin changed under mobile breakpoint

--- src/features/MoviePage/Content/styled.js
+++ src/features/MoviePage/Content/styled.js
@@ -4,4 +4,12 @@ export const MoviesContainer = styled.div`
   max-width: 1400px;
   padding: 0 16px;
   margin: 64px auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    margin: 40px auto;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin: 24px auto;
+  }
 `;
