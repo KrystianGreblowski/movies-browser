9c72a7e1c865ba395430bb19996203326863afe4
Damian Niklasinski <damianiklasinski@wp.pl>
2024-04-03 13:00:58 +0200

    Temporarily commented Switch in App.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,21 +1,22 @@
 import {
   HashRouter,
-  Redirect,
-  Route,
-  Switch,
+  //Redirect,
+  //Route,
+  //Switch,
 } from "react-router-dom/cjs/react-router-dom";
 import NavigationBar from "../common/NavigationBar";
-import NoResults from "../features/NoResults";
-import MainPage from "../features/MainPage";
-import PersonList from "../features/PersonList";
-import { toMainPage, toPersonList } from "./routes";
-import MoviesDetails from "../features/MoviesDetails";
+import PersonDetails from "../features/PersonDetails";
+//import NoResults from "../features/NoResults";
+//import MainPage from "../features/MainPage";
+//import PersonList from "../features/PersonList";
+//import { toMainPage, toPersonList } from "./routes";
+//import MoviesDetails from "../features/MoviesDetails";
 
 function App() {
   return (
     <HashRouter>
       <NavigationBar />
-      <Switch>
+      {/* <Switch>
         <Route path="/movies/:title">
           <MoviesDetails />
         </Route>
@@ -28,7 +29,8 @@ function App() {
         <Route path="/">
           <Redirect to="/movies" />
         </Route>
-      </Switch>
+  </Switch> */}
+      <PersonDetails />
     </HashRouter>
   );
 }
