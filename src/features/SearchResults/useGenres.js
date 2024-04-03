a8f3c3c5720936863f6545fc74c7e28568a896d9
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-04-03 23:32:09 +0200

    useGenres created

---
+++ src/features/SearchResults/useGenres.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const apiKey = "?api_key=5b5837d1c9bb80f117b57d1d4ef3d375";
+const url = `https://api.themoviedb.org/3/genre/movie/list${apiKey}`;
+
+export const useGenres = () => {
+  const [genres, setGenres] = useState({
+    data: [],
+    status: "loading",
+  });
+
+  useEffect(() => {
+    const getGenres = async () => {
+      try {
+        const response = await axios.get(url);
+        setGenres({
+          status: "success",
+          data: response.data.genres,
+        });
+      } catch (err) {
+        setGenres({
+          status: "error",
+        });
+        console.error(err.message);
+      }
+    };
+    setTimeout(getGenres, 0);
+  }, []);
+
+  return { genres };
+};
