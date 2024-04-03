9a3d406408014ea0ab448f03b76d87574371a344
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-04-03 23:33:13 +0200

    useSearchResults created, re-wrotten, and than again re-wrotten....

--- src/features/SearchResults/useSearchResults.js
+++ src/features/SearchResults/useSearchResults.js
@@ -1,10 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom/cjs/react-router-dom";
+import { useQueryParameter } from "../../common/NavigationBar/SearchBar/queryParameters";
+import axios from "axios";
 
 export const useSearchResults = () => {
-    const [searchResults, setSearchResults] = useState([]);
-    const location = useLocation();
-    
+  const query = useQueryParameter("search");
+  const apiKey = "5b5837d1c9bb80f117b57d1d4ef3d375";
+  const location = useLocation();
+  const isMoviesPage = location.pathname.startsWith("/movies");
+    const searchParams = new URLSearchParams(location.search);
+  const currentPage = parseInt(searchParams.get("page")) || 1;
 
+  const [searchResults, setSearchResults] = useState({
+    status: "",
+    data: [],
+  });
+  useEffect(() => {
+    setSearchResults({
+      status: "loading",
+      data: [],
+    });
 
-}
+    const debounce = setTimeout(async () => {
+      if (query) {
+        const url = `https://api.themoviedb.org/3/search/${
+          isMoviesPage ? "movie" : "person"
+        }?api_key=${apiKey}&language=en-US&query=${query}&page=${currentPage}`;
+        try {
+          const response = await axios.get(url);
+          setSearchResults({
+            status: "success",
+            data: response.data,
+          });
+        } catch (error) {
+          setSearchResults({
+            status: "error",
+          });
+          console.log(error.message);
+        }
+      }
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [query, isMoviesPage, currentPage]);
+
+  return { searchResults };
+};
