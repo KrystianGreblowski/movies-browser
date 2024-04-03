4e3af6e6e388b48bb06591c5405e5c6cf6e96c7f
Damian Niklasinski <damianiklasinski@wp.pl>
2024-04-03 16:42:52 +0200

    Section cast added

---
+++ src/features/PersonDetails/Cast/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CastContainer = styled.div`
+  max-width: 1368px;
+  margin: auto;
+  margin-top: 64px;
+`;
+
+export const CastTitle = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 43px;
+`;
--- src/features/PersonDetails/index.js
+++ src/features/PersonDetails/index.js
@@ -1,10 +1,12 @@
 import AboutPerson from "./AboutPerson";
+import Cast from "./Cast";
 import { DetailsContainer } from "./styled";
 
 function PersonDetails() {
   return (
     <DetailsContainer>
       <AboutPerson />
+      <Cast />
     </DetailsContainer>
   );
 }
