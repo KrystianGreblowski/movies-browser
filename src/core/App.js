c2091a7ab1550306f2a1e4643f8a0ee252981cc0
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-04 22:05:59 +0200

    toMovieDetails added to routes.js

--- src/core/App.js
+++ src/core/App.js
@@ -7,7 +7,7 @@ import {
 import NavigationBar from "../common/NavigationBar";
 import MainPage from "../features/MainPage";
 import PersonList from "../features/PersonList";
-import { toMainPage, toPersonList } from "./routes";
+import { toMainPage, toMovieDetails, toPersonList } from "./routes";
 import MoviePage from "../features/MoviePage";
 import ScrollToTop from "../common/ScrollToTop";
 
@@ -19,7 +19,7 @@ function App() {
       <NavigationBar />
 
       <Switch>
-        <Route path="/movies/:movieId">
+        <Route path={toMovieDetails()}>
           <MoviePage />
         </Route>
 
--- src/core/routes.js
+++ src/core/routes.js
@@ -1,3 +1,5 @@
 export const toPersonList = () => "/personlist";
 
 export const toMainPage = () => "/movies";
+
+export const toMovieDetails = () => "/movies/:movieId";
