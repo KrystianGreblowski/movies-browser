6ebad0fd78c9c1c41332a85576c7ba4d57ed0b1b
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-04-05 16:22:50 +0200

    Created common Container Folder

---
+++ src/common/Container/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 0 16px;
+  margin-bottom: 103px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-bottom: 31px;
+  }
+`;
