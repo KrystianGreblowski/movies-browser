df749e13891a6c49b0c4ac408308e0bbc57ec7e6
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-06 11:05:15 +0200

    NoResults corrected

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,17 +1,20 @@
 import styled from "styled-components";
-import { ReactComponent as Picture } from "../../images/Picture.svg"
+import { ReactComponent as Picture } from "../../images/Picture.svg";
 
 export const NoResultsImage = styled(Picture)`
-    width: 668px;
-    height: auto;
-    display: flex;
-    justify-content: center;
-    position: relative;
-    margin: auto;
+  width: 668px;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin: auto;
 `;
 
 export const NoResultsWrapper = styled.div`
-    font-weight: 600;
-    font-size: 36px;
-    margin-bottom: 40px;
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 0 16px;
+  font-weight: 600;
+  font-size: 36px;
+  margin-bottom: 40px;
 `;
