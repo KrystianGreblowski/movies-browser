70d289037cbfed424a234329622258e0cf196b7b
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-06 11:18:44 +0200

    NoResults page corrected

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,20 +1,29 @@
 import styled from "styled-components";
-import { ReactComponent as Picture } from "../../images/Picture.svg";
-
-export const NoResultsImage = styled(Picture)`
-  width: 668px;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  position: relative;
-  margin: auto;
-`;
 
 export const NoResultsWrapper = styled.div`
   max-width: 1400px;
   margin: 0 auto;
   padding: 0 16px;
-  font-weight: 600;
-  font-size: 36px;
-  margin-bottom: 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-bottom: 31px;
+  }
+`;
+
+export const NoResultsImage = styled.img`
+  display: block;
+  margin: 0 auto;
+  max-width: 668px;
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    width: 400px;
+    margin-top: 0;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+    width: 250px;
+  }
 `;
