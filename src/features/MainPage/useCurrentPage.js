f4841849dadc7d1b94175c7ddfa389e5261f17e3
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-06 18:48:05 +0200

    useCurrentPage added

---
+++ src/features/MainPage/useCurrentPage.js
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { selectCurrentPage } from "../../common/Pagination/paginationSlice";
+import { fetchCurrentMoviesPage } from "./popularMoviesSlice";
+
+export const useCurrentPage = () => {
+  const dispatch = useDispatch();
+  const currentPage = useSelector(selectCurrentPage);
+
+  useEffect(() => {
+    const getPageNumberForApi = () =>
+      currentPage === 1 ? 1 : Math.floor((currentPage - 1) / 5) * 2 + 1;
+
+    if ((currentPage - 1) % 5 === 0)
+      dispatch(fetchCurrentMoviesPage(getPageNumberForApi()));
+  }, [currentPage, dispatch]);
+
+  return currentPage;
+};
