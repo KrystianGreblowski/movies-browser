6fe4077d12c4a59c5f3b53faecd20e57a6f2e9da
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-10 14:36:00 +0200

    styles for AboutPerson corrected

--- src/features/PersonDetails/AboutPerson/styled.js
+++ src/features/PersonDetails/AboutPerson/styled.js
@@ -2,18 +2,26 @@ import styled from "styled-components";
 
 export const AboutPersonContainer = styled.div`
   display: grid;
+  grid-template-columns: auto 1fr;
   grid-gap: 40px;
-  max-width: 1368px;
   margin: 56px auto;
   background-color: ${({ theme }) => theme.color.white};
   padding: 40px;
-  grid-template-columns: auto 1fr;
+  box-shadow: 0px 4px 12px 0px #bac7d580;
+  border-radius: 5px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    margin-top: 0px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    grid-gap: 32px;
+    padding: 32px;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-gap: 24px;
+    padding: 24px;
+    margin: 32px auto;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
     grid-gap: 16px;
     padding: 16px;
     margin: 24px auto;
@@ -21,7 +29,7 @@ export const AboutPersonContainer = styled.div`
 `;
 
 export const PersonImage = styled.img`
-  max-width: 500px;
+  max-width: 400px;
   width: 100%;
   height: auto;
   object-fit: cover;
@@ -29,66 +37,87 @@ export const PersonImage = styled.img`
   grid-row: span 2;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
-    max-width: 300px;
+    max-width: 250px;
+    grid-row: auto;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    max-width: 250px;
+    max-width: 180px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
-    max-width: 200px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    max-width: 100px;
-    grid-row: auto;
+    max-width: 116px;
   }
 `;
 
 export const PersonDescription = styled.div`
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    margin-top: 0px;
-    margin-left: 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  justify-content: space-evenly;
+  margin-top: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    justify-content: start;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    margin-top: 5px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
+    gap: 16px;
+    margin-top: 4px;
   }
 `;
 
 export const Caption = styled.h1`
   font-weight: 600;
   font-size: 36px;
+  line-height: 43.2px;
   margin: 0;
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 32px;
+    line-height: 36px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    font-size: 22px;
+    font-size: 30px;
+    line-height: 32px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22.2px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    font-size: 18px;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 18.2px;
   }
 `;
 
 export const Details = styled.div`
   display: flex;
+  gap: 8px;
   flex-direction: column;
-  margin-top: 24px;
 `;
 
 export const BirthDetails = styled.div`
   display: flex;
   flex-direction: row;
-  margin-top: -25px;
   gap: 10px;
+  flex-wrap: wrap;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    flex-direction: column;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    gap: 8px;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
-    flex-direction: column;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    gap: 6px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    gap: 4px;
   }
 `;
 
@@ -96,70 +125,52 @@ export const Span = styled.p`
   color: ${({ theme }) => theme.color.stormGray};
   font-weight: 400;
   font-size: 18px;
+  line-height: 21.6px;
+  margin: 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    font-size: 14px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    margin-top: 30px;
-    font-size: 12px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 16px;
+    line-height: 19px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
-    margin-top: 30px;
+    font-size: 12px;
+    line-height: 14.4px;
   }
 `;
 
 export const SpanInfo = styled.p`
   font-size: 18px;
   font-weight: 400;
+  line-height: 21.6px;
+  margin: 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    font-size: 14px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    margin-top: -20px;
-    font-size: 12px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 16px;
+    line-height: 19px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
-    margin-top: -20px;
+    font-size: 12px;
+    line-height: 14.4px;
   }
 `;
 
 export const PersonInformation = styled.p`
   font-weight: 400;
   font-size: 20px;
-  line-height: 28px;
-  margin-top: -226px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallLaptop}) {
-    margin-top: -226px;
-  }
+  line-height: 32px;
+  margin: 0;
+  text-align: justify;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    grid-column: span 2;
     font-size: 18px;
-    margin-top: -140px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallTablet}) {
-    font-size: 15px;
-    margin-top: -120px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    font-size: 14px;
-    margin-top: 0px;
+    line-height: 24px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.largeMobile}) {
-    grid-column: span 2;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mediumMobile}) {
-    font-size: 12px;
-    line-height: 25px;
+    font-size: 14px;
+    line-height: 22.4px;
   }
 `;
