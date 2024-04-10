272e9057b9a5d154c07d54367169f4eaa6ea1c22
Oleksandr Riaboshapka <134525143+Alekzann@users.noreply.github.com>
2024-04-11 00:30:24 +0200

    ErrorPage don't work here...

--- src/features/MoviePage/Content/index.js
+++ src/features/MoviePage/Content/index.js
@@ -2,15 +2,33 @@ import MainInfo from "./MainInfo";
 import Cast from "./Cast";
 import Crew from "./Crew";
 import { MoviesContainer } from "./styled";
+import LoadingPage from "../../../common/LoadingPage";
+import { selectMovieDetailsStatus } from "../movieDetailsSlice";
+import { useSelector } from "react-redux";
+import { Container } from "../../../common/Container/styled";
+import ErrorPage from "../../../common/ErrorPage";
 
 const Content = () => {
+
+  const movieDetailsStatus = useSelector(selectMovieDetailsStatus)
   return (
-    <MoviesContainer>
-      <MainInfo />
-      <Cast />
-      <Crew />
-    </MoviesContainer>
+    <>
+      {movieDetailsStatus === "loading" ? (
+        <LoadingPage />
+      ) : movieDetailsStatus === "success" ? (
+        <MoviesContainer>
+          <MainInfo />
+          <Cast />
+          <Crew />
+        </MoviesContainer>
+      ) : movieDetailsStatus === "error" ? (
+        <Container>
+          <ErrorPage />
+        </Container>
+      ) : (
+        <ErrorPage/>
+      )}
+    </>
   );
 };
-
 export default Content;
