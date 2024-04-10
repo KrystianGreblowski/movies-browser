bdbab589654cf5367cce7d946b03d1d2c25f1969
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-10 15:42:10 +0200

    margin-bottom added

--- src/features/PersonDetails/styled.js
+++ src/features/PersonDetails/styled.js
@@ -3,5 +3,14 @@ import styled from "styled-components";
 export const DetailsContainer = styled.div`
   max-width: 1400px;
   margin: 0 auto;
+  margin-bottom: 220px;
   padding: 0 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    margin-bottom: 120px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-bottom: 32px;
+  }
 `;
