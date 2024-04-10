c814d409560dd21efec77b423b866059dc5109d8
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-10 15:34:06 +0200

    MoviePageLink added to Cast

--- src/features/PersonDetails/Cast/index.js
+++ src/features/PersonDetails/Cast/index.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { nanoid } from "nanoid";
 import { MovieTile } from "../../../common/Tiles/MovieTilesContainer/MovieTile/index";
 import noMovieImage from "../../../images/no-movie-image.png";
@@ -6,8 +6,11 @@ import { TilesContainer } from "../../../common/Tiles/MovieTilesContainer/styled
 import { TilesHeader } from "../../../common/Tiles/TilesHeader/styled";
 import { selectPersonDetailsData } from "../personDetailsSlice";
 import { useMovieTypesData } from "../../../api/useMovieTypesData";
+import { MoviePageLink } from "../../../common/MoviePageLink/styled";
+import { fetchMovieId } from "../../MoviePage/movieDetailsSlice";
 
 const Cast = () => {
+  const dispatch = useDispatch();
   const personDetailsData = useSelector(selectPersonDetailsData);
   const { movieTypesData, numberOfMovieTypes } = useMovieTypesData();
 
@@ -18,30 +21,36 @@ const Cast = () => {
       <TilesHeader>Movies - cast ({numberOfTiles})</TilesHeader>
       <TilesContainer>
         {personDetailsData.movie_credits.cast.map((cast) => (
-          <MovieTile
+          <MoviePageLink
+            to={`/movies/${cast.id}`}
+            onClick={() => dispatch(fetchMovieId(cast.id))}
             key={nanoid()}
-            image={
-              cast.poster_path === null
-                ? noMovieImage
-                : "https://image.tmdb.org/t/p/w300" + cast.poster_path
-            }
-            title={cast.original_title}
-            year={
-              cast.character && cast.release_date
-                ? `${cast.character} (${cast.release_date.slice(0, 4)})`
-                : cast.character
-                ? cast.character
-                : cast.release_date
-                ? `(${cast.release_date.slice(0, 4)})`
-                : ""
-            }
-            type={movieTypesData
-              .filter((movieType) => cast.genre_ids.includes(movieType.id))
-              .map((movieType) => movieType.name)
-              .slice(0, numberOfMovieTypes)}
-            rate={cast.vote_average.toFixed(1).toString().replace(".", ",")}
-            votes={cast.vote_count}
-          />
+          >
+            <MovieTile
+              key={nanoid()}
+              image={
+                cast.poster_path === null
+                  ? noMovieImage
+                  : "https://image.tmdb.org/t/p/w300" + cast.poster_path
+              }
+              title={cast.original_title}
+              year={
+                cast.character && cast.release_date
+                  ? `${cast.character} (${cast.release_date.slice(0, 4)})`
+                  : cast.character
+                  ? cast.character
+                  : cast.release_date
+                  ? `(${cast.release_date.slice(0, 4)})`
+                  : ""
+              }
+              type={movieTypesData
+                .filter((movieType) => cast.genre_ids.includes(movieType.id))
+                .map((movieType) => movieType.name)
+                .slice(0, numberOfMovieTypes)}
+              rate={cast.vote_average.toFixed(1).toString().replace(".", ",")}
+              votes={cast.vote_count}
+            />
+          </MoviePageLink>
         ))}
       </TilesContainer>
     </>
