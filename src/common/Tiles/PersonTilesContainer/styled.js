572703434db66ebca5c0d65579f71c1ba69e1922
Krystian Greblowski <krystian.greblowski@gmail.com>
2024-04-13 19:57:07 +0200

    number of tiles corrected

--- src/common/Tiles/PersonTilesContainer/styled.js
+++ src/common/Tiles/PersonTilesContainer/styled.js
@@ -6,12 +6,8 @@ export const PersonTilesContainer = styled.div`
   grid-template-columns: repeat(5, 1fr);
   margin-bottom: 56px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallLaptop}) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
   @media (max-width: ${({ theme }) => theme.breakpoint.smallTablet}) {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
